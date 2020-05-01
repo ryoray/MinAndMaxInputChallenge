3c54b1a43ff59b1e9e1d3e319d6e537126dfc79a
ryoray <praretno.wibowo@gmail.com>
2020-05-02 06:34:31 +0700

    Add handler for checking first number and assign the value as current min and max numbers

--- src/academy/learnprogramming/Main.java
+++ src/academy/learnprogramming/Main.java
@@ -7,9 +7,11 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanInput = new Scanner(System.in);
 
+        int minNumber = 0;
         int maxNumber = 0;
         int checkNumber;
         int counter = 1;
+        boolean first = true;
 
         while (true) {
             System.out.print("Enter number #" + counter + ": ");
@@ -19,16 +21,26 @@ public class Main {
 
             if (hasIntVal) {
                 checkNumber = scanInput.nextInt();
+
+                if (first) {
+                    minNumber = checkNumber;
+                    maxNumber = checkNumber;
+                    first = false;
+                }
                 if (checkNumber > maxNumber) {
                     maxNumber = checkNumber;
                 }
+                if (checkNumber < minNumber) {
+                    minNumber = checkNumber;
+                }
             } else {
-                System.out.println("Invalid Value");
                 break;
             }
             scanInput.nextLine();
         }
+        System.out.println("------------------------");
+        System.out.println("The min number is " + minNumber);
         System.out.println("The max number is " + maxNumber);
-        scanInput.close();;
+        scanInput.close();
     }
 }
